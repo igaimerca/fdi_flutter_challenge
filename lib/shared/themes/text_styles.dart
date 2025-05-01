e49e03be0abd4b0ae31b8826f95076881528b60d
Aime Igirimpuhwe <igaimerca@gmail.com>
2025-05-01 10:52:14 +0200

    feat: project setup

--- lib/shared/themes/text_styles.dart
+++ lib/shared/themes/text_styles.dart
@@ -1,3 +1,4 @@
+import 'package:fdi_challenge/shared/themes/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -5,30 +6,36 @@ class AppTextStyles {
   static final heading = GoogleFonts.ibmPlexSans(
     fontSize: 22,
     fontWeight: FontWeight.w600,
-    color: Colors.black,
+    color: AppColors.black,
   );
 
   static final subheading = GoogleFonts.ibmPlexSans(
     fontSize: 18,
     fontWeight: FontWeight.w500,
-    color: Colors.black,
+    color: AppColors.black,
   );
 
   static final body = GoogleFonts.roboto(
     fontSize: 14,
     fontWeight: FontWeight.w400,
-    color: Colors.black,
+    color: AppColors.black,
   );
 
   static final bodyMuted = GoogleFonts.roboto(
     fontSize: 14,
     fontWeight: FontWeight.w400,
-    color: const Color(0xFF7F8790),
+    color: AppColors.textMuted1,
   );
 
   static final label = GoogleFonts.roboto(
     fontSize: 13,
     fontWeight: FontWeight.w500,
-    color: Colors.black,
+    color: AppColors.black,
+  );
+
+  static final poppinsBold = GoogleFonts.poppins(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    color: AppColors.black,
   );
 }
